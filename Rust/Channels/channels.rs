3232c2f6c66088e0c0d08dab517b02c4184f9f66
Edmore Moyo <edmore.moyo@allangray.co.za>
2020-03-22 22:09:34 +0200

    fix: updates the Rust channels example

--- Rust/Channels/channels.rs
+++ Rust/Channels/channels.rs
@@ -2,24 +2,18 @@ use std::thread;
 use std::sync::mpsc;
 
 fn main() {
-    let (tx, rx) = mpsc::channel(); // async channel - "infinite buffer" - no send will block, recv will blcok until a message is available
+    let (tx, rx) = mpsc::channel();
 
-    // we’re using thread::spawn to create a new thread and then using move to move tx into the closure so the spawned thread owns tx
-    // The spawned thread needs to own the transmitting end of the channel to be able to send messages through the channel.
     thread::spawn(move || {
-        let greeting = String::from("hi");
+        let coffee_order = String::from("1, Espresso");
 
-        // must be considered safe to send between threads
-        // unwrap is used to panic if there is an error, in this case - should be handled correctly in a real application
-        tx.send(greeting).unwrap();
+        tx.send(coffee_order).unwrap();
 
         // Allowing this would be a bad idea: once the value has been sent to another thread,
         // that thread could modify or drop it before we try to use the value again.
-        // println!("greeting is {}", greeting);
+        println!("Brewing ... {}", coffee_order);
     });
 
-    // The receiving end of a channel has two useful methods: recv and try_recv. 
-    // We’re using recv, short for receive, which will block the main thread’s execution and wait until a value is sent down the channel.
     let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    println!("Brewing ... {}", received);
 }
