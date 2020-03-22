dd9ccaebe0aa7187164ae2ba1b9d18e40e0db31d
Edmore Moyo <edmore.moyo@allangray.co.za>
2020-03-22 22:11:22 +0200

    fix: renames vars

--- Rust/Channels/channels.rs
+++ Rust/Channels/channels.rs
@@ -2,18 +2,18 @@ use std::thread;
 use std::sync::mpsc;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (sender, receiver) = mpsc::channel();
 
     thread::spawn(move || {
         let coffee_order = String::from("1, Espresso");
 
-        tx.send(coffee_order).unwrap();
+        sender.send(coffee_order).unwrap();
 
         // Allowing this would be a bad idea: once the value has been sent to another thread,
         // that thread could modify or drop it before we try to use the value again.
         println!("Brewing ... {}", coffee_order);
     });
 
-    let received = rx.recv().unwrap();
+    let received = receiver.recv().unwrap();
     println!("Brewing ... {}", received);
 }
