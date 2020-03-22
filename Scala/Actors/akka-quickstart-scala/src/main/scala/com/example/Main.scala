56022556578e1d7d6d757ad854c04f1a507c482c
Edmore Moyo <edmore.moyo@allangray.co.za>
2020-03-22 18:17:24 +0200

    fix: updates AKKA code to coffee world example

---
+++ Scala/Actors/akka-quickstart-scala/src/main/scala/com/example/Main.scala
@@ -0,0 +1,62 @@
+package com.example
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import Barista._
+import Printer._
+
+// Barista companion object
+object Barista {
+  def props(printerActor: ActorRef): Props = Props(new Barista(printerActor))
+
+  final case class WhatToMake(typeOfCoffee: String, number: Int)
+
+  case object MakePayment
+
+}
+
+class Barista(printerActor: ActorRef) extends Actor {
+  var order = "" // state
+
+  def receive = {
+    case WhatToMake(typeOfCoffee, number) =>
+      order = number + ", " + typeOfCoffee // updates the "order" state
+    case MakePayment =>
+      printerActor ! PrintReceipt(order) // print receipt
+  }
+}
+
+// printer companion object
+object Printer {
+  def props: Props = Props[Printer]
+
+  final case class PrintReceipt(order: String)
+
+}
+
+class Printer extends Actor with ActorLogging { // extends logger as well
+  def receive = {
+    case PrintReceipt(order) =>
+      log.info("Payment received (from " + sender() + "): " + order) // prints receipt
+  }
+}
+
+object Main extends App {
+  // Create the 'CoffeeWorld' actor system
+  val system: ActorSystem = ActorSystem("CoffeeWorld")
+
+  // Create printer actor
+  val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+  // Create the 'coffee drinker' actors
+  val espressoLover: ActorRef =
+    system.actorOf(Barista.props(printer), "espressoLover")
+  val flatWhiteLover: ActorRef =
+    system.actorOf(Barista.props(printer), "flatWhiteLover")
+
+  // send messages
+  espressoLover ! WhatToMake("Espresso", 1)
+  espressoLover ! MakePayment
+
+  flatWhiteLover ! WhatToMake("Flat White", 2)
+  flatWhiteLover ! MakePayment
+}
+
